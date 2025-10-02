570fa932c257774a8b7291373a0cb154aed74a6a
kowsalya2025 <kowsalyacse1992@gmail.com>
2025-10-03 00:10:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'your_email@gmail.com'
-    MAIL_PASSWORD = 'your_email_password'
+    MAIL_USERNAME = 'kowsalyacse1992@gmail.com'  
+    MAIL_PASSWORD = 'mljunabrnedeunpx'  
